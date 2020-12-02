ba6e45db07e756031c49446b6aa6f3469ab1f4d5
arsen <asmandiyarov_ars@mail.ru>
2020-12-03 04:06:45 +0500

    3rd HW Asmandiyarov

--- django_framework/settings.py
+++ django_framework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'django_framework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'mainapp/templates']
-        ,
+        'DIRS': [BASE_DIR / 'mainapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -124,3 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
         BASE_DIR / 'static',
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-
-links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classics', 'name': 'классика'},
-        ]
+from mainapp.models import Product, ProductCategory
 
 
 def main(request):
+    products = Product.objects.all()[:3]
     content = {
-        'title': 'Главная'
+        'title': 'Главная',
+        'products': products
     }
     return render(request, 'mainapp/index.html', content)
 
@@ -23,49 +18,12 @@ def contact(request):
     return render(request, 'mainapp/contact.html', content)
 
 
-def product(request):
+def product(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+
     content = {
         'title': 'Продукты',
         'links_menu': links_menu,
     }
     return render(request, 'mainapp/products.html', content)
 
-
-def products_all(request):
-    content = {
-        'title': 'продукты',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', content)
-
-
-def products_home(request):
-    content = {
-        'title': 'продукты',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', content)
-
-
-def products_office(request):
-    content = {
-        'title': 'продукты',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', content)
-
-
-def products_modern(request):
-    content = {
-        'title': 'продукты',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', content)
-
-
-def products_classics(request):
-    content = {
-        'title': 'продукты',
-        'links_menu': links_menu,
-    }
-    return render(request, 'mainapp/products.html', content)
